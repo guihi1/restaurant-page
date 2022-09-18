02e673e9a774127603ba7af1d42f67a106778748
guihi1 <gui.hikio@gmail.com>
2022-09-18 14:08:35 -0300

    Move part of the homeDisplay function here

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import './styles.css'
+import { menuDisplay } from './menu';
+import { homeDisplay } from './home';
+import { contactDisplay } from './contact';
+
+const title = document.createElement("h1");
+title.textContent = "Restaurant";
+document.getElementById("content").appendChild(title);
+
+const nav = document.createElement("nav");
+document.getElementById("content").appendChild(nav);
+
+const unList = document.createElement("ul");
+nav.appendChild(unList);
+const homeLink = document.createElement("li");
+homeLink.textContent = "Home";
+const menuLink = document.createElement("li");
+menuLink.textContent = "Menu";
+const contactLink = document.createElement("li");
+contactLink.textContent = "Contact";
+unList.appendChild(homeLink);
+unList.appendChild(menuLink);
+unList.appendChild(contactLink);
+
+homeDisplay();
+menuDisplay();
+contactDisplay();
+
+homeLink.addEventListener("click", () => {
+    if(document.getElementById("contact").classList.contains("hide") === false){
+        document.getElementById("contact").classList.add("hide");
+    }
+    if(document.getElementById("menu").classList.contains("hide") === false){
+        document.getElementById("menu").classList.add("hide");
+    }
+    document.getElementById("home").classList.remove("hide");
+});
+
+menuLink.addEventListener("click", () => {
+    if(document.getElementById("contact").classList.contains("hide") === false){
+        document.getElementById("contact").classList.add("hide");
+    }
+    if(document.getElementById("home").classList.contains("hide") === false){
+        document.getElementById("home").classList.add("hide");
+    }
+    document.getElementById("menu").classList.remove("hide");
+});
+
+contactLink.addEventListener("click", () => {
+    if(document.getElementById("menu").classList.contains("hide") === false){
+        document.getElementById("menu").classList.add("hide");
+    }
+    if(document.getElementById("home").classList.contains("hide") === false){
+        document.getElementById("home").classList.add("hide");
+    }
+    document.getElementById("contact").classList.remove("hide");
+});
