d5d969646f000b616dc5ec60961fa6b86e469fe8
guihi1 <gui.hikio@gmail.com>
2022-09-18 18:49:08 -0300

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
